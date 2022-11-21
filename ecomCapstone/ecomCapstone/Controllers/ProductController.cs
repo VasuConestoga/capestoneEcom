70c2820df32ea25ffd0928cfdc53b24a8b4a7283
Vasu Panchal <Vpanchal9922@conestogac.on.ca>
2022-11-21 17:20:54 -0500

    admin add product

--- ecomCapstone/ecomCapstone/Controllers/ProductController.cs
+++ ecomCapstone/ecomCapstone/Controllers/ProductController.cs
@@ -30,7 +30,7 @@ namespace ecomCapstone.Controllers
                     "PM.Description as ProductDescription , PM.Quantity , PM.Price , PM.ImgUrl as ProductImage,CM.Name as CategoryName " +
                     "from ProductMaster PM " +
                     "INNER JOIN ProductCatgoryMapping PCM on PM.ID = PCM.ProductId " +
-                    "INNER JOIN CategoryMaster CM on CM.ID = PCM.CategoryId where 1=1  ";
+                    "INNER JOIN CategoryMaster CM on CM.ID = PCM.CategoryId where 1=1 and [isActive] = 1 ";
 
                 if (!String.IsNullOrEmpty(input.searchtext) )
                 {
@@ -82,5 +82,126 @@ namespace ecomCapstone.Controllers
             }
             return op;
         }
+
+        [HttpPost]
+        [Route("AddProduct")]
+        public Response AddProduct(ProductList product)
+        {
+            Response op = new Response();
+            try
+            {
+                bool active = true;
+                DateTime createdOn = DateTime.UtcNow;
+
+                SqlConnection con = new SqlConnection(_configuration.GetConnectionString("EcomCon").ToString());
+                SqlCommand cmd = new SqlCommand("INSERT INTO[dbo].[ProductMaster] ([Name],[Description],[ImgUrl],[Quantity],[Price],[isActive],[CreatedOn])" +
+                    "VALUES ('" + product.name + "','" 
+                                + product.description + "','" 
+                                + product.imageUrl + "','" 
+                                + product.quantity + "','" 
+                                + product.price + "','"
+                                + active + "','"
+                                + createdOn + "')", con);
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                if (i > 0)
+                {
+                    op.Success = true;
+                    op.Message = "Product Added Successfully";
+                }
+                else
+                {
+                    op.Success = false;
+                    op.Message = "Error while saving";
+                }
+            }
+            catch (Exception ex)
+            {
+                op.Success = false;
+                op.Message = ex.Message.ToString();
+
+            }
+            return op;
+        }
+
+        [HttpPost]
+        [Route("UpdateProduct")]
+        public Response UpdateProduct(ProductList product)
+        {
+            Response op = new Response();
+            try
+            {
+                DateTime UpdatedOn = DateTime.UtcNow;
+
+                SqlConnection con = new SqlConnection(_configuration.GetConnectionString("EcomCon").ToString());
+                SqlCommand cmd = new SqlCommand("UPDATE [dbo].[ProductMaster]  SET [Name] = '" + product.name 
+                    + "',[Description] = '" + product.description
+                    + "',[ImgUrl] = '" + product.imageUrl 
+                    + "',[Quantity] ='" + product.quantity 
+                    + "',[Price] ='" + product.price 
+                    + "',[UpdatedOn] ='" + UpdatedOn
+                    + "' WHERE Id = '" + product.Id + "'", con);
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                if (i > 0)
+                {
+                    op.Success = true;
+                    op.Message = "Product Updated Successfully";
+                }
+                else
+                {
+                    op.Success = false;
+                    op.Message = "Error while saving";
+                }
+            }
+            catch (Exception ex)
+            {
+                op.Success = false;
+                op.Message = ex.Message.ToString();
+
+            }
+            return op;
+        }
+
+
+        [HttpPost]
+        [Route("DeleteProduct")]
+        public Response DeleteProduct(ProductList product)
+        {
+            Response op = new Response();
+            try
+            {
+                bool active = false;
+                DateTime UpdatedOn = DateTime.UtcNow;
+
+                SqlConnection con = new SqlConnection(_configuration.GetConnectionString("EcomCon").ToString());
+                SqlCommand cmd = new SqlCommand("UPDATE [dbo].[ProductMaster]  SET [isActive] = '" + active
+                    + "',[UpdatedOn] ='" + UpdatedOn
+                    + "' WHERE Id = '" + product.Id + "'", con);
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                if (i > 0)
+                {
+                    op.Success = true;
+                    op.Message = "Product Deleted Successfully";
+                }
+                else
+                {
+                    op.Success = false;
+                    op.Message = "Error while saving";
+                }
+            }
+            catch (Exception ex)
+            {
+                op.Success = false;
+                op.Message = ex.Message.ToString();
+
+            }
+            return op;
+        }
+
     }
 }
